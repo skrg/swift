b976a70e24a4160b753b4d2b6e6f19873b86d4a4
saku <sakuchi@sakudeMacBook-Air.local>
2014-09-19 15:13:52 +0800

    Initial Commit

---
+++ myfirstiosapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfirstiosapp
+//
+//  Created by saku on 14/9/19.
+//  Copyright (c) 2014年 saku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
